7eff5b6d412e74abcc8fd5656286f0c60c8953c4
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-20 21:14:00 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -61,7 +61,7 @@ def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
-  matrix[1][1]="new_value"
+  matrix[1][1]=new_value
   
   matrix 
   
