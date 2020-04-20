4b2a87dd2c8b48ec0823156ae662f503d6d30073
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-20 20:32:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -23,7 +23,7 @@ def assembled_matrix
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
   
   
-  assembled =[]
+  assembled =[ORGANIC_PRODUCE , CONVENTIONAL_PRODUCE ]
 end
 
 def sorted_matrix
